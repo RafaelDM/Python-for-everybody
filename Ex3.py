cdf29e6f973b5beafcfc8480b5f04535fa465faa
Nayra1316 <RafaelDM@users.noreply.github.com>
2020-09-14 18:16:09 -0500

    Exercise from class 3.3

---
+++ Ex3.py
@@ -0,0 +1,32 @@
+"""
+Rafael Díaz Medina Science Computer student from Tecnologico de Monterrey
+
+3.3 Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range, print an error.
+If the score is between 0.0 and 1.0, print a grade using the following table:
+Score Grade
+>= 0.9 A
+>= 0.8 B
+>= 0.7 C
+>= 0.6 D
+< 0.6 F
+If the user enters a value out of range, print a suitable error message and exit. For the test, enter a score of 0.85.
+"""
+
+score= input("Give me a score: ")
+try:
+    valscore=float(score)
+except:
+    print("Invalid input")
+    quit()
+if valscore<0.6:
+    print("F")
+elif valscore>= 0.9:
+    print("A")
+elif valscore>=0.8:
+    print("B")
+elif valscore>=0.7:
+    print("C")
+elif valscore>= 0.6:
+    print("D")
+else:
+    print("Invalid input")
